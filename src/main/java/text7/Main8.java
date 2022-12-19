01000dc883f03c49472ad42d677fecb2d244b925
donoun6 <donoun6@naver.com>
2022-12-19 23:07:23 +0900

    20221219

--- src/main/java/text7/Main8.java
+++ src/main/java/text7/Main8.java
@@ -1,53 +1,28 @@
 package text7;
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-
+ 
 public class Main8 {
-
+ 
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int weight = N;
-		int num = 0;
-		int ans = 0;
-		while (weight != 0) {
-			if((weight%5)%3==0) {
-				ans += weight/5;
-				weight = weight%5;
-				ans += weight/3;
-				weight = weight%3;
-			}else {
-				weight -= 5;
-				num += 5;
-				if(weight < 0) {
-					weight = N;
-					break;
-				}
-				if(weight%3 == 0) {
-					ans = weight/3;
-					weight = (weight%3)+num;
-				}
-			}
+		
+ 
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
 		}
-		while(weight != 0) {
-			if(weight%3 == 0) {
-				ans += weight/3;
-				weight = weight%3;
-			}else {
-				weight -= 3;
-				num += 3;
-				if(weight < 0) {
-					ans = -1;
-					break;
-				}
-				if((weight%3)%3 == 0) {
-					ans += weight/3;
-					weight = (weight%3)+3;
-				}
-			}
+		else if (N % 5 == 0) {
+			System.out.println(N / 5);
 		}
-		System.out.println(ans);
+		else if (N % 5 == 1 || N % 5 == 3) {
+			System.out.println((N / 5) + 1);
+		}
+		else if (N % 5 == 2 || N % 5 == 4) {
+			System.out.println((N / 5) + 2);
+		}
 	}
 }
