6a59d71acccaf200ff7354d956edd2472a42cf99
donoun6 <donoun6@naver.com>
2023-08-29 22:16:29 +0900

    20230829

---
+++ src/main/java/test/Main15.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+		System.out.println(a+b+c);
+	}
+
+}
